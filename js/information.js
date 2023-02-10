43dcc67af2f5f0f9351be4b6bf7b59d6f3a0e7a4
PennyWays <109055996+PennyWays@users.noreply.github.com>
2023-02-11 00:06:39 +0400

    create new change

---
+++ js/information.js
@@ -0,0 +1,42 @@
+const Information = [
+  {
+    id: 1,
+    name: "Заголовок 1",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, maiores.",
+  },
+  {
+    id: 2,
+    name: "Заголовок 2",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, maiores.",
+  },
+  {
+    id: 3,
+    name: "Заголовок 3",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, maiores.",
+  },
+  {
+    id: 4,
+    name: "Заголовок 4",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, maiores.",
+  },
+  {
+    id: 5,
+    name: "Заголовок 5",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, maiores.",
+  },
+  {
+    id: 6,
+    name: "Заголовок 6",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, maiores.",
+  },
+  {
+    id: 7,
+    name: "Заголовок 7",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, maiores.",
+  },
+  {
+    id: 8,
+    name: "Заголовок 8",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, maiores.",
+  },
+];
--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
 });
 
 const ROOT_Catalog = document.getElementById("catalog-wrapper");
+const ROOT_Information = document.getElementById("information-cards-wrapper");
 
 class Products {
   render() {
@@ -32,5 +33,27 @@ class Products {
   }
 }
 
+class InformationCards {
+  render() {
+    let htmlInformation = "";
+    Information.forEach((element) => {
+      htmlInformation += `
+      <div class="information-cards">
+          <div class="information-title-wrapper">
+            <h1 class="information-title">${element["name"]}</h1>
+          </div>
+          <p class="information-p">
+            ${element["text"]}
+          </p>
+        </div>
+      `;
+      ROOT_Information.innerHTML = htmlInformation;
+    });
+  }
+}
+
 const products = new Products();
 products.render();
+
+const information = new InformationCards();
+information.render();
